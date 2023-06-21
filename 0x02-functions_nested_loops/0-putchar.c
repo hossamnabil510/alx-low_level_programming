967cbe28e995f28f33c200618b7043c5c981b480
hossamnabil510 <hossamnabil510@gmail.com>
2023-06-20 17:32:29 -0700

    gg

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+/**
+ * main - entry point
+ * return - zero sucess
+ */
+int main()
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
