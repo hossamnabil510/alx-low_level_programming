2135983cb67769a35345e8194200fa79dd913d7e
hossamnabil510 <hossamnabil510@gmail.com>
2023-06-23 13:08:26 -0700

    daa

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet in lowercase
+ *
+ * Return: 0 is sucessful
+*/
+
+int main(void)
+{
+	int mychar = 0;
+
+	while (mychar <= 9)
+	{
+		putchar(mychar);
+		mychar++;
+	}
+	putchar('\n');
+	return (0);
+}
