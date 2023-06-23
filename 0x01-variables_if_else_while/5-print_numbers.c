8ed13f9ae10ffb2d456c1b4b2aeac7c43d7c2262
hossamnabil510 <hossamnabil510@gmail.com>
2023-06-23 13:01:49 -0700

    daa

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet in lowercase
+ *
+ * Return: 0 is sucessful
+*/
+
+int main(void)
+{
+	int mychar = 0;
+
+	while (mychar <= 9)
+	{
+		printf("%i /n", mychar);
+		mychar++;
+	}
+	return (0);
+}
