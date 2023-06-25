4c1d1d8590ae8ece820feac25d31969957ad352b
hossamnabil510 <hossamnabil510@gmail.com>
2023-06-25 15:16:04 -0700

    aa

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabet - used to print from a to z using putchar
+ * print_alphabet_x10 - used to print from a to z using putchar
  * Return: 0 is sucessful
  */
 
 void print_alphabet_x10(void)
 {
 	int c, d;
+
 	for (d = 0; d < 10; d++)
 	{
 		for (c = 'a'; c < 'z'; c++)
