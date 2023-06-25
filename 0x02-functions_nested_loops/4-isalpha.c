df53ad820db153bcea6c9a99583c8c2f10e4ed8c
hossamnabil510 <hossamnabil510@gmail.com>
2023-06-25 16:27:14 -0700

    a

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,10 +5,10 @@
  * Return: 1 is lowercase oterwise is 0
 */
 
-int _islower(int c)
+int _isalpha(int c)
 {
 
-	if (c >= 'a' && c <= 'z' && c>= 'A' && c <= 'Z')
+	if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z')
 		return (1);
 	return (0);
 }
