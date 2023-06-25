6c40d156194f1ec44755bbe1ca64ca7ce022ec99
hossamnabil510 <hossamnabil510@gmail.com>
2023-06-25 16:19:25 -0700

    a

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - is a function to determine lowercase
+ * @c: check the input
+ * Return: 1 is lowercase oterwise is 0
+*/
+
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'z' && c>= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
