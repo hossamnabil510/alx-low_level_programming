b82ab1db07eda1b1472b6cd2dca082dcc49d22da
hossamnabil510 <hossamnabil510@gmail.com>
2023-06-25 13:46:54 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * Description: _putchar using putchar prototype
+ * Return: zero sucess
+*/
+
+int main(void)
+{
+	int c = 97
+		while(c <=122 )
+		{
+			_putchar(c);
+			c++
+		}
+			_putchar('\n');
+	return (0);
+}
