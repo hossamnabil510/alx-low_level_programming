dc713178503f9bfc9ef0e0b7d00653c43ec20a5a
hossamnabil510 <hossamnabil510@gmail.com>
2023-06-30 18:21:23 -0700

    gg

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
 /**
- * reverse_array - reverse array of ints
- * @a: array value
- * @n: number of array elements
- * Return: void
+ * string_toupper - change lowercase char to uppercase
+ * @n: pointer
+ * Return: n
 */
 
-void reverse_array(int *a, int n)
+char *string_toupper(char *)
 {
 	int i;
-	
+
 	i = 0;
-		while (n[i] != '\0')
-		{
-		
-			for (n[i] >= 'a' && n[i] <= 'z')
-				n[i] = n[i] - 32;
-			n++;
-		}
-		return (n);
+	while (n[i] != '\0')
+	{
+		for (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
 }
