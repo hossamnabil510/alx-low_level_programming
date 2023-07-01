0f01b08e6e4e94bd5c147e0a5fe417811b0560c2
hossamnabil510 <hossamnabil510@gmail.com>
2023-06-30 19:32:24 -0700

    gg

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize al strings
+ * @str: capitalize string
+ * Return: pointer changed to string
+*/
+
+char *cap_string(char *)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str [index] <= 'z'))
+			index++;
+
+		if(str[index - 1] == ' ' || str[index -1] == '\t' || str[index -1] == '\n' || str[index -1] == ',' || str[index -1] == ';' || str[index -1] == '.' || str[index -1] == '!' || str[index -1] == '?' || str[index -1] == '"' || str[index -1] == '(' || str[index -1] == ')' || str[index -1] == '{' || str[index -1] == '}' || index == 0)
+			str[index] -= 32;
+		index++;
+	}
+	return (str);
+}
