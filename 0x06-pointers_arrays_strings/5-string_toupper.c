31687f9afcf5b17b8216ad6533c8d710cd43f838
hossamnabil510 <hossamnabil510@gmail.com>
2023-06-30 18:15:20 -0700

    gg

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array of ints
+ * @a: array value
+ * @n: number of array elements
+ * Return: void
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	
+	i = 0;
+		while (n[i] != '\0')
+		{
+		
+			for (n[i] >= 'a' && n[i] <= 'z')
+				n[i] = n[i] - 32;
+			n++;
+		}
+		return (n);
+}
