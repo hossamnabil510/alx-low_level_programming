c61dcf1f4710aa34592c8a416a615fc299bcd36b
hossamnabil510 <hossamnabil510@gmail.com>
2023-06-30 18:58:57 -0700

    gg

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,14 +6,14 @@
  * Return: n
 */
 
-char *string_toupper(char *)
+char *string_toupper(char *n)
 {
 	int i;
 
 	i = 0;
 	while (n[i] != '\0')
 	{
-		for (n[i] >= 'a' && n[i] <= 'z')
+		if (n[i] >= 'a' && n[i] <= 'z')
 			n[i] = n[i] - 32;
 		i++;
 	}
