cc857831228d69afd8f7f77518a838e039558a45
hossamnabil510 <hossamnabil510@gmail.com>
2023-06-30 18:05:32 -0700

    gg

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * @a: array
+ * reverse_array - reverse array of ints
+ * @n: num of array elements
+ * Return: void
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int t;
+
+		for (i = 0; i < n--; i++)
+		{
+			t = a[i];
+			a[i] = a[n];
+			a[n] = t;
+		}
+}
