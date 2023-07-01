ad2b4cc65504244cc4a202ddc072d3fc45d9d8c6
hossamnabil510 <hossamnabil510@gmail.com>
2023-06-30 17:36:56 -0700

    gg

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings
+ * _strncat - concatenates two strings using n byte from src
  * @dest: input value
  * @src: output value
  * $@n: input value
