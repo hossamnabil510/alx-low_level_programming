ba4bb73206ca5cf245996499679a3b15b47a4512
hossamnabil510 <hossamnabil510@gmail.com>
2023-07-05 15:33:50 -0700

    gg

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+i#include "main.h"
+
+/**
+ * _print_rev_recursion - print reserved strings
+ * @s: reverse string
+ * Return: nothing
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	s++;
+	_print_rev_recursion(s);
+	s++;
+	_putchar(*s);
+}
