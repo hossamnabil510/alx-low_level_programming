c5e76640d1c97a71b819d6108073c302cf494108
hossamnabil510 <hossamnabil510@gmail.com>
2023-07-10 11:51:02 -0700

    gg

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+
+#include "main.h"
+/**
+ * print_last_digit - is a function to determine positive and negative nums
+ * @n: check the input
+ * Return: 0 is lowercase or  oterwise is 0
+*/
+
+int print_last_digit(int n)
+{
+	printf("%i", n 10 %);
+	return (0);
+}
