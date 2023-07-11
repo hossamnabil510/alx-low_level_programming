7455eef78d96215853b0aa0fea737ef7067b42c7
hossamnabil510 <hossamnabil510@gmail.com>
2023-07-11 14:08:01 -0700

    g

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+
+/**
+ * print_numbers - cheack if the character is upper or lower
+ * @c: input char
+ * Return: if uppercase 0 otherwise
+*/
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+		_putchar(c);
+	_putchar("\n");
+	return (0);
+}
