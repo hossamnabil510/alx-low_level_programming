2429686e4397b07729fc1d1fb0efe0857e71be97
hossamnabil510 <hossamnabil510@gmail.com>
2023-07-11 15:16:34 -0700

    g

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+
+#include "main.h"
+
+/**
+ * more_numbers - cheack if the character is upper or lower
+ * Return: if uppercase 0 otherwise
+*/
+
+void more_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '10'; c++)
+	{
+		for (c = '0'; c <= '14'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
