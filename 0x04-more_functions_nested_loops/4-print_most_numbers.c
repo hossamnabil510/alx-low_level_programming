65a4e414846569dbe47878388e82020db3678e65
hossamnabil510 <hossamnabil510@gmail.com>
2023-07-11 14:42:02 -0700

    g

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - cheack if the character is upper or lower
+ * Return: if uppercase 0 otherwise
+*/
+
+void print_most_numbers(void)
+{
+	for (c = 48; c <= 57; c++)
+	{
+		while (c != 50 || c != 52)
+			_putchar(c);
+	}
+	_putchar('\n');
+}
