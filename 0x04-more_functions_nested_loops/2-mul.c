fe7194dc509bccf9ca8fdfdeb99e48494cdcdb2c
hossamnabil510 <hossamnabil510@gmail.com>
2023-07-11 13:57:52 -0700

    ggg

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,17 +1,14 @@
-
-
 #include "main.h"
 
 /**
  * mul - cheack if the character is upper or lower
- * @c: input char
- * Return: 1 if uppercase 0 otherwise
+ * @a: input char
+ * @b: input char
+ * @c: output char
+ * Return: c if uppercase 0 otherwise
 */
 
 int mul(int a, int b)
 {
-	int c;
-
-	c = a * b;
-	return c;
+	return (a * b);
 }
