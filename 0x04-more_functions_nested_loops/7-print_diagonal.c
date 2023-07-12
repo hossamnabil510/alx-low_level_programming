44b70917ecf600553e06ae783e8f0bbc517ca4e7
hossamnabil510 <hossamnabil510@gmail.com>
2023-07-11 18:03:15 -0700

    g

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,9 +18,9 @@ void print_diagonal(int n);
 		for (c = 1 ; c <= n; c++)
 		{
 			for (d = 1 ; d <= c; d++)
-			_putchar(' ');
-		_putchar(92);
-		_putchar('\n');
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
 		}
 	}
 
