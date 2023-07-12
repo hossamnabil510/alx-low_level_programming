773173361676b0b408e35cc6cd7d765e5887c70d
hossamnabil510 <hossamnabil510@gmail.com>
2023-07-11 20:11:44 -0700

    g

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,9 +11,9 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (n % 3 == 0 !n % 5 == 0)
+		if (n % 3 == 0 && !(n % 5 == 0))
 			printf("Fizz");
-		if (n % 5 == 0 !n % 3 == 0)
+		if (n % 5 == 0 && !(n % 3 == 0))
 			printf("Buzz");
 		if (n % 15 == 0)
 			printf("FizzBuzz");
