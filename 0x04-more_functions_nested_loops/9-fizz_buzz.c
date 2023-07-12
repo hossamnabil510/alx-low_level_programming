eb65e5deaa04e83e8e11cacbc3e9635b635a4d8c
hossamnabil510 <hossamnabil510@gmail.com>
2023-07-11 19:43:45 -0700

    g

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - cheack if the character is upper or lower
+ * Return: 0 umber of _
+*/
+
+int main()
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n = 3**)
+			_printf("Fizz");
+		else if (n = 3**)
+			_printf("Buzz");	
+		else if (n = 15**)
+			_printf("FizzBuzz");
+		else
+			_printf("%i", n);
+	}
+}
+
