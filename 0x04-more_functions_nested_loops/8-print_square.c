370f47fa2eeaccf7f48a93e9c5fd44a082610163
hossamnabil510 <hossamnabil510@gmail.com>
2023-07-11 19:04:14 -0700

    g

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+
+
+#include "main.h"
+
+/**
+ * print_square - cheack if the character is upper or lower
+ * @size: number of _
+ * Return: if uppercase 0 otherwise
+*/
+
+void print_square(int size)
+{
+	int c, d;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (c = 1 ; c <= size; c++)
+		{
+			_putchar(35);
+			for (d = 1 ; d <= c; d++)
+				_putchar(35);
+			_putchar('\n');
+		}
+	}
+}
+
