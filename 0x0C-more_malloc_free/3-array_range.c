476783737c63cc859173498c6609f858c54cb671
hossamnabil510 <hossamnabil510@gmail.com>
2023-07-13 19:44:31 -0700

    g

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 		return (NULL);
 	len = max - min + 1;
 
-	ptr r = malloc(sizeof(int) * len);
+	ptr = malloc(sizeof(int) * len);
 
 	if (!ptr)
 		return (NULL);
