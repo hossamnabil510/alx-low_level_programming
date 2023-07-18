fa6ac1a058ffd677d300fb88bc5db59d3460abbf
hossamnabil510 <hossamnabil510@gmail.com>
2023-07-17 19:56:17 -0700

    f

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * _strlen - a dog struct
+ * @str: initial the dog
+ * Return: void
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int length = 0;
+
+	while (*str++)
+		length++;
+	return (length);
+}
